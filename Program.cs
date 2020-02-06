03588e61be708c526ed62ebed28445eb1df9a510
Thomas Houghton <tomhoughton888@gmail.com>
2020-02-06 11:34:01 +0000

    Created Initial Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VotingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
